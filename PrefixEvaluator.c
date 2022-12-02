542a22d3e20897df1776f758e79e857660cc8cf8
Max Base <maxbasecode@gmail.com>
2022-12-03 00:37:00 +0100

    create a file

---
+++ PrefixEvaluator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    char *data;
+    int top;
+    int size;
+} Stack;
